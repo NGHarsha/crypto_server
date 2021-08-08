531da3d78e421644b6fdb6181f403021ece85334
NGHarsha <harshavardhanng99@gmail.com>
2021-08-08 15:04:19 +0530

    first commit

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const cors = require("cors");
 const coinRouter = require("./routes/coins.js");
 const userRouter = require("./routes/users");
 
+const dotenv = require("dotenv").config();
+
 app.use(cors());
 
 app.use("/api/coins", coinRouter);
@@ -24,13 +26,13 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || "An unknown error occurred!" });
 });
 
+const port = process.env.PORT || 5000;
+
 mongoose
-  .connect(
-    "mongodb+srv://Aryastark:aryastark@cluster0.dgucn.mongodb.net/test?retryWrites=true&w=majority"
-  )
+  .connect(process.env.MONGODB_URI)
   .then(() => {
-    app.listen(5000, () => {
-      console.log("Server listening on 5000");
+    app.listen(port, () => {
+      console.log("Server listening on " + port);
     });
   })
   .catch((err) => {
